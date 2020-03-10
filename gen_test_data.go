9cf44bdf769ff0392a273ba24060e6656c66a3a4
li.li <li.li@greatopensource.com>
2020-03-10 12:33:09 +0800

    add gen_test_data.go

---
+++ gen_test_data.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "time"
+    "math/rand"
+)
+
+
+func main() {
+    str := "abcdefghijklmnopqrstuvwxyz"
+    bytes := []byte(str)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 10000000; i++ {
+        result := []byte{}
+        length := r.Intn(20)
+        for j := 0; j < length ; j++ {
+            result = append(result, bytes[r.Intn(len(bytes))])
+        }
+        fmt.Println(string(result))
+    }
+}
